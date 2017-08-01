070ce421f40b46f4a1059033f37c616369e99af4
Edparra <Edprr4@gmail.com>
2017-07-31 22:59:54 -0400

    article validation

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: { minimun: 3 , maximum: 50 }
+  validates :description, presence: true, length: { minimun: 3 , maximum: 50 }
+
+end
